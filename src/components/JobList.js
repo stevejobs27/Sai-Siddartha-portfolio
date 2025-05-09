87e5ad9e0e8081e846ad2f9d8d949136832d3e20
Rafsan Ahmed <rafsanahmed2828@gmail.com>
2025-05-09 18:56:24 -0400

    Update

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -82,15 +82,15 @@ const JobList = () => {
   const [value, setValue] = React.useState(0);
 
   const experienceItems = {
-    Amazon: {
-      jobTitle: "Software Development Engineer @",
-      duration: "JUL 2022 - PRESENT",
+    Google: {
+      jobTitle: "Data Analyst @",
+      duration: "JUL 2025 - PRESENT",
       desc: [
-        "Led development of end-to-end region build automation across Route 53 (AWS's DNS web service).  This enabled the launch of customer-facing global services in new regions within a day, a significant reduction from the previous time-frame of a month.",
-        "Re-built Route 53's core domain management and DNS systems to provide a better user experience to millions of customers."
+        "Loremed ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi."
       ]
     },
-    Wattpad: {
+    IBM: {
       jobTitle: "Associate Engineer @",
       duration: "MAY 2020 - APR 2021",
       desc: [
@@ -99,17 +99,17 @@ const JobList = () => {
         "Collaborated with senior engineers and product management following best practices for the full software development life cycle, including coding standards, code reviews, source control management, build processes, testing, and operations."
       ]
     },
-    "University of Toronto": {
-      jobTitle: "Research Engineer @",
-      duration: "MAY 2021 - SEPT 2021",
+    "Innovation Boost Zone": {
+      jobTitle: "Project Lead @",
+      duration: "SEP 2022 - DEC 2023",
       desc: [
         "Developed and researched an NLP-based framework using state-of-the-art tools like Spacy and Stanza to facilitate the derivation of requirements from health data by leveraging syntactic dependencies, entity-recognition and rule-based match-making.",
         " Application selected for DCS Research Award ($4,000) as part of the ”Visualizing Privacy Analysis Results” project led by Professor Marsha Chechik."
       ]
     },
-    Centivizer: {
-      jobTitle: "Software Developer @",
-      duration: "SEPT 2019 - APR 2020",
+    "ShieldMate INC.": {
+      jobTitle: "Research Analyst @",
+      duration: "MAR 2023 - DEC 2023",
       desc: [
         "Developed interactive and neural-activation technologies to stimulate physical and cognitive functions in order to slow the progression of neurodegenerative disorders.",
         "Leveraged WebRTC to develop and maintain a Node.js online video-streaming platform in real-time competitive-mode games to research the effects of active stimulation for those suffering from dementia."
@@ -122,9 +122,9 @@ const JobList = () => {
     //     "Co-developed homework management software integrable with Google Classroom by utilizing the Python’s Flask micro-framework for the back-end API and Vue.js for the front-end UI, in order to translate business requirements into a functional full-stack application."
     //   ]
     // },
-    "Orange Gate": {
-      jobTitle: "Software Developer Intern @",
-      duration: "MAY 2019 - AUG 2019",
+    HealthMate: {
+      jobTitle: "Technical Lead @",
+      duration: "SEP 2022 - FEB 2023",
       desc: [
         "Developed a Node.js smart home system through Facebook’s Messenger integrated with Bocco sensors and other smart devices (Nest camera, TPLink smart plugs) to derive conclusions about the current state of the home",
         "Identified continuous improvements in data quality, design reports and coding activities, presenting results and findings to internal business stakeholders.",
