52d2b96462daa6865aa65df37331d365d17d6865
Rafsan Ahmed <rafsanahmed2828@gmail.com>
2025-05-29 11:23:42 -0400

    New Intro!

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,73 +1,71 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../styles/Intro.css";
-import Typist from "react-typist";
-import "react-typist/dist/Typist.css";
 import FadeInSection from "./FadeInSection";
-import CircleAnimations from "./CircleAnimations";
+import AnimatedRobot from "./AnimatedRobot";
+import { FiMail } from "react-icons/fi"; 
+import Typist from "react-typist";
+import { gsap } from "gsap";
 
-const spiralIds = ["anim1", "anim3", "anim5", "anim6", "anim8", "anim9", "anim10", "anim11", "anim12"];
+const Intro = () => {
+  const cursorRef = useRef(null);
 
-class Intro extends React.Component {
-  constructor() {
-    super();
-    const defaultId = "anim8";
-    const randomId = spiralIds[Math.floor(Math.random() * spiralIds.length)];
-
-    this.state = {
-      expanded: true,
-      activeKey: "1",
-      visible: true,
-      randomSpiralId: window.performance.navigation.type === 1 ? randomId : defaultId,
-      showPopup: false, // <-- add popup state
-    };
-
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleCircleClick = this.handleCircleClick.bind(this);
-    this.handlePopup = this.handlePopup.bind(this);
-    this.closePopup = this.closePopup.bind(this);
-  }
-
-  handleSelect(eventKey) {
-    this.setState({
-      activeKey: eventKey,
+  useEffect(() => {
+    // Animate the windmill cursor
+    gsap.to(cursorRef.current.querySelector('svg'), {
+      rotation: 360,
+      repeat: -1,
+      duration: 10,
+      ease: "none",
+      transformOrigin: "center center"
     });
-  }
 
-  handleCircleClick() {
-    let newId;
-    do {
-      newId = spiralIds[Math.floor(Math.random() * spiralIds.length)];
-    } while (newId === this.state.randomSpiralId);
-    this.setState({ randomSpiralId: newId });
-  }
+  }, []);
 
-  handlePopup() {
-    this.setState({ showPopup: true });
-  }
-
-  closePopup() {
-    this.setState({ showPopup: false });
-  }
-
-  render() {
-    return (
-      <div id="intro">
-        <CircleAnimations
-          showid={this.state.randomSpiralId}
-          onCircleClick={this.handleCircleClick}
-        />
-        <Typist
-          avgTypingDelay={120}
-          cursor={{ show: true, blink: true, hideWhenDone: true, hideWhenDoneDelay: 0 }}
-        >
-          <span className="intro-title">
-            {"hey, there! i'm  "}
-          </span>
-            <span className="intro-name">{"rafsan."}</span>
-        </Typist>
-        <div className="intro-subtitle">
-          I'm a <span className="intro-subtitle-name">{"Data Analyst"}</span> with a love for design.
+  return (
+    <div className="intro-section">
+      <div className="intro-content">
+        <div className="typist-content">
+          <Typist 
+            avgTypingDelay={70}
+            stdTypingDelay={25}
+            cursor={{
+              show: false, // Hide default cursor since we have our custom one
+              blink: true,
+              element: '|'
+            }}
+          >   
+            <span className="intro-title">Hello there! I'm </span>
+            <span className="intro-name">Rafsan.</span>
+            <span>&nbsp;</span>
+            <Typist.Delay ms={300} />
+          </Typist>
+          
+          <div 
+            ref={cursorRef} 
+            id="custom-cursor" 
+            className="windmill-cursor"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 248 248" aria-hidden="true">
+              <path fill="url(#windmill-gradient-1)" d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"></path>
+              <path fill="url(#windmill-gradient-2)" d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"></path>
+              <defs>
+                <linearGradient id="windmill-gradient-1" x1="218" x2="-47.283" y1="258" y2="153.706" gradientUnits="userSpaceOnUse">
+                  <stop offset=".27" stopColor="#64D98A"></stop>
+                  <stop offset=".838" stopColor="#ccd6f6"></stop>
+                </linearGradient>
+                <linearGradient id="windmill-gradient-2" x1="-21.183" x2="223.712" y1="-7.807" y2="329.472" gradientUnits="userSpaceOnUse">
+                  <stop offset=".27" stopColor="#64D98A"></stop>
+                  <stop offset=".838" stopColor="#ccd6f6"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
         </div>
+        
+        <div className="intro-subtitle">
+          I'm a <span className="intro-subtitle-name">Data Analyst</span> with a love for design.
+        </div>
+        
         <FadeInSection>
           <div className="intro-desc">
             I'm an aspiring Data Analyst based in Toronto, Canada. Passionate about using data to
@@ -75,9 +73,19 @@ class Intro extends React.Component {
             industries to create meaningful impact.
           </div>
         </FadeInSection>
+        
+        <FadeInSection>
+          <div className="intro-buttons">
+            <a href="#contact" className="outline-button">Get In Touch <FiMail className="button-icon" /></a>
+          </div>
+        </FadeInSection>
       </div>
-    );
-  }
-}
+      
+      <div className="intro-animation">
+        <AnimatedRobot />
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
